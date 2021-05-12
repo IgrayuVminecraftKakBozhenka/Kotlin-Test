1005e93ab34de9458a38bf13b0c0b250e6e4f0d1
danil <danko200028@mail.ru>
2021-05-12 16:36:42 +0600

    question done

--- app/src/main/java/com/example/kotlintest/TestFragment.kt
+++ app/src/main/java/com/example/kotlintest/TestFragment.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.kotlintest.data.QuestionAndAnswerDao
 import com.example.kotlintest.data.QuestionAndAnswerDatabase
 import com.example.kotlintest.data.QuestionModel
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
 class TestFragment() : Fragment() {
 
     lateinit var mContext: Context
+    private var dao: QuestionAndAnswerDao? = null
+    private var questions: ArrayList<QuestionModel> = ArrayList()
+    private var questionIndex = 0
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -33,20 +38,36 @@ class TestFragment() : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        dao =QuestionAndAnswerDatabase.getDatabase(mContext).dao()
         val question = view.findViewById<TextView>(R.id.question_fragment_question)
-        var questions: List<QuestionModel>
+        val nextButton = view.findViewById<Button>(R.id.question_fragment_button_next)
+
+        getQuestionFromDb(question)
+
+
+        nextButton.setOnClickListener {
+            if (questionIndex < questions.size) {
+                question.text = questions[questionIndex++].question
+            } else
+             Toast.makeText(mContext, "все", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun getQuestionFromDb(question: TextView) {
         GlobalScope.launch {
-            delay(2000)
             val data = async(Dispatchers.IO) {
-                QuestionAndAnswerDatabase.getDatabase(mContext).dao().readAllQuestionData()
+                dao?.readAllQuestionData()
             }
             withContext(Dispatchers.Main) {
-                questions = data.await()
-                question.text = questions[0].question
+                val result = data.await()
+                if (!result.isNullOrEmpty()) {
+                    questions.addAll(result)
+                    question.text = questions[questionIndex].question
+                    questionIndex++
+                }
             }
         }
-        //val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
-        //question.text = questions[0].question
-
     }
 }
