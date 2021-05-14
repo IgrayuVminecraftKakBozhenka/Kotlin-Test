e3bb7684b348e73da75e290ba84566534c76bb22
danil <danko200028@mail.ru>
2021-05-14 17:09:58 +0600

    first commit

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -4,22 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlintest.MainFragment.OnBeginButtonPressed
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnBeginButtonPressed {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
-        val beginButton = findViewById<Button>(R.id.begin_button)
-        beginButton.setOnClickListener {
-            val intent = Intent(this, TestActivity::class.java)
-            startActivity(intent)
+        val mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, mainFragment)
+            .commit()
+    }
 
-
-        }
-
-
+    override fun onButtonPressed() {
+        val testFragment = TestFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, testFragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/kotlintest/MainFragment.kt
@@ -0,0 +1,35 @@
+package com.example.kotlintest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment() {
+
+    interface OnBeginButtonPressed {
+        fun onButtonPressed()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val beginButton = view.findViewById<Button>(R.id.begin_button)
+
+        beginButton.setOnClickListener {
+            val listener = activity as OnBeginButtonPressed?
+            listener?.onButtonPressed()
+        }
+
+    }
+}
